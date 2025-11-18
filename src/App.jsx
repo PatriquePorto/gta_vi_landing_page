f3d1c0a2044c15006e0879470333aa5f424bcf70
Patrique Porto <patrikrock@hotmail.com>
2025-11-18 18:15:44 -0300

    Add hero section animated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import Hero from './sections/Hero'
+import NavBar from './sections/NavBar'
 
 gsap.registerPlugin(ScrollTrigger)
-
 
 const App = () => {
   return (
     <main>
-        <h1 className='text-4xl text-pink'>Welcome to the world of GTA and GSAP!</h1>
+        <NavBar />
+        <Hero />
     </main>
   )
 }
