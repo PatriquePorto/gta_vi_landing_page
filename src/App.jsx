920855722b5c5a7afcce9b5e47dd12704eb206fb
Patrique Porto <patrikrock@hotmail.com>
2025-12-06 10:43:39 -0300

    Add section vid lucia

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Hero from "./sections/Hero";
 import NavBar from "./sections/NavBar";
 import FirstVideo from "./sections/FirstVideo";
 import Jason from "./sections/Jason";
-import ViceText from "./sections/ViceText";
+import SecondVideo from "./sections/SecondVideo";
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
@@ -16,6 +16,7 @@ const App = () => {
       <Hero />
       <FirstVideo />
       <Jason />
+      <SecondVideo />
     </main>
   );
 };
