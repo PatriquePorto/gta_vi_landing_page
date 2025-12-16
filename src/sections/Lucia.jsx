3f37a323bcded10b3ba2b858bb69596fd4d0325a
Patrique Porto <patrikrock@hotmail.com>
2025-12-16 11:37:38 -0300

    Implement second video and align text section

--- src/sections/Lucia.jsx
+++ src/sections/Lucia.jsx
@@ -1,40 +1,8 @@
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
+import React from 'react'
 
-const Jason = () => {
-  useGSAP(() => {
-    gsap.set(".jason", { marginTop: "-80vh" });
-
-    gsap
-      .timeline({
-        scrollTrigger: {
-          trigger: ".jason",
-          start: "top 90%",
-          end: "10% center",
-          scrub: 2,
-        },
-      })
-      .to(".first-vd", { opacity: 0, duration: 1, ease: "power1.inOut" });
-
-    gsap.to(
-      ".jason .img-box",
-      {
-        scrollTrigger: {
-          trigger: ".jason",
-          start: "top center",
-          end: "80% center",
-          scrub: 2,
-        },
-        y: -300,
-        duration: 1,
-        ease: "power1.inOut",
-      },
-      "<"
-    );
-  });
-
+const Lucia = () => {
   return (
-    <section className="jason">
+    <section className="lucia-life">
       <div className="max-w-lg jason-content">
         <h1>Jason Duval</h1>
         <h2>Jason wants an easy life, but things just keep getting harder.</h2>
@@ -50,7 +18,7 @@ const Jason = () => {
         </div>
       </div>
 
-      <div className="space-y-5 mt-100 img-box">
+      <div className="space-y-5 mt-96 img-box">
         <div className="jason-1">
           <img src="/images/jason-1.webp" alt="jason" />
         </div>
@@ -59,7 +27,7 @@ const Jason = () => {
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Jason;
+export default Lucia
