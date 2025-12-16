e46ef188c24f6b94bd32297da40345f024b4e5ec
Patrique Porto <patrikrock@hotmail.com>
2025-12-16 16:32:08 -0300

    Implement section update

--- src/sections/Lucia.jsx
+++ src/sections/Lucia.jsx
@@ -3,7 +3,7 @@ import { useGSAP } from "@gsap/react";
 
 const Lucia = () => {
   useGSAP(() => {
-    gsap.set('.lucia-life', { marginTop: '-80vh'})
+    gsap.set('.lucia-life', { marginTop: '-50vh'})
 
     gsap.timeline({
       scrollTrigger: {
@@ -26,7 +26,7 @@ const Lucia = () => {
   return (
     <section className="lucia-life">
       <div className="flex flex-col gap-5 items-end img-box lg:1/2 ps-10 mt-96">
-        <div className="lucia-1">
+        <div className="lucia-1 w-full h-dvh">
           <img src="/images/lucia-1.webp" alt="lucia" />
         </div>
         <div className="lucia-3">
@@ -35,25 +35,23 @@ const Lucia = () => {
       </div>
 
          <div className="lg:w-1/2 lucia-life-content">
-         <div className='max-w-xl lg:ps-32 ps-10'>
-            <h1>Lucia Caminos</h1>
-            <h2>Lucia's father taught her to fight as soon as she could walk.</h2>
-            <p>
-              Life has been coming at her swinging ever since. Fighting for her family landed her 
-              in the Leonida Panitentiary. Sheer luck got her out. Lucia's learned her lesson - 
-              only smart moves from here.
-            </p>
-         </div>
-     
+            <div className='max-w-xl lg:ps-32 w-full ps-10'>
+                <h1>Lucia Caminos</h1>
+                <h2>Lucia's father taught her to fight as soon as she could walk.</h2>
+                <p>
+                 Life has been coming at her swinging ever since. Fighting for her family landed her in the Leonida Penitentiary. Sheer luck got her out. Lucia’s learned her lesson — only smart moves from here.
+                </p>
+            </div>
+        
 
-        <div className="lucia-2">
-          <img src="/images/lucia-2.webp" alt="lucia" />
-        </div>
+            <div className="lucia-2 w-full h-[70vh] ">
+              <img src="/images/lucia-2.webp" alt="lucia" />
+            </div>
 
-          <p className='max-w-xl lg:ps-32 ps-10'>
-            More than anything, Lucia wants the good life her mon has dreamed of since their days in Liberty City - 
-            but instead of half-baked fantasies, Lucia is prepared to take matters into her own hands.
-          </p>
+              <p className='max-w-xl lg:ps-32 ps-10'>
+                More than anything, Lucia wants the good life her mon has dreamed of since their days in Liberty City - 
+                but instead of half-baked fantasies, Lucia is prepared to take matters into her own hands.
+              </p>
       </div>
     </section>
   )
